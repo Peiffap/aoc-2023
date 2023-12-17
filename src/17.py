6e9c6d70ada17b90712ec56d4f0c908e9214aba7
Gilles Peiffer <gilles.peiffer.yt@gmail.com>
2023-12-17 09:35:31 +0100

    Add Python solution for 2023 Day 17

---
+++ src/17.py
@@ -0,0 +1,115 @@
+from collections import defaultdict
+
+fo = open("../data/17.txt", "r")
+f = list(fo)
+fo.close()
+
+grid = []
+for l in f:
+    grid.append([int(i) for i in l.strip()])
+
+seen = defaultdict(lambda: 4)
+
+q = [(0, 0, 0, 0, (0, 1), [(0, 0)])] # total cost, row, col, #consec, direction
+node = q[0]
+m = 0
+while q:
+    q = q[1:]
+    if node[0] > m:
+        #print(node[0])
+        m = node[0]
+    if 0 <= node[1] < len(grid[0]) and 0 <= node[2] < len(grid) and node[3] < seen[node[1], node[2], node[4]]:
+        if node[1] == len(grid[0]) - 1 and node[2] == len(grid) - 1:
+            print(node[0])
+            break
+        cnt = 0
+        path = []
+        for nsteps in range(1, 3 + 1 - node[3]):
+            nrow = node[1] + nsteps * node[4][0]
+            ncol = node[2] + nsteps * node[4][1]
+            if 0 <= nrow < len(grid[0]) and 0 <= ncol < len(grid):
+                cnt += grid[nrow][ncol]
+                path.append((nrow, ncol))
+                q.append((node[0] + cnt, nrow, ncol, node[3] + nsteps, node[4], node[5] + path.copy()))
+            else:
+                break
+        if node[4] == (1, 0) or node[4] == (-1, 0):
+            for ndir in [(0, 1), (0, -1)]:
+                cnt = 0
+                path = []
+                for nsteps in range(1, 3 + 1):
+                    nrow = node[1] + nsteps * ndir[0]
+                    ncol = node[2] + nsteps * ndir[1]
+                    if 0 <= nrow < len(grid[0]) and 0 <= ncol < len(grid):
+                        cnt += grid[nrow][ncol]
+                        path.append((nrow, ncol))
+                        q.append((node[0] + cnt, nrow, ncol, nsteps, ndir, node[5] + path.copy()))
+                    else:
+                        break
+        elif node[4] == (0, 1) or node[4] == (0, -1):
+            for ndir in [(1, 0), (-1, 0)]:
+                cnt = 0
+                path = []
+                for nsteps in range(1, 3 + 1):
+                    nrow = node[1] + nsteps * ndir[0]
+                    ncol = node[2] + nsteps * ndir[1]
+                    if 0 <= nrow < len(grid[0]) and 0 <= ncol < len(grid):
+                        cnt += grid[nrow][ncol]
+                        path.append((nrow, ncol))
+                        q.append((node[0] + cnt, nrow, ncol, nsteps, ndir, node[5] + path.copy()))
+                    else:
+                        break
+        seen[(node[1], node[2], node[4])] = node[3]
+        for x in q:
+            if seen[x[1], x[2], x[4]] < x[3]:
+                q.remove(x)
+        q.sort()
+    node = q[0]
+
+def mmin(s):
+    if len(s) == 0:
+        return 11
+    return min(s)
+
+def mmax(s):
+    if len(s) == 0:
+        return -1
+    return max(s)
+
+seen = defaultdict(set)
+
+q = [(0, 0, 0, 0, (0, 1)), (0, 0, 0, 0, (1, 0))] # total cost, row, col, #consec, direction
+node = q[0]
+m = 0
+while q:
+    q.remove(node)
+    if node[0] > m:
+        #print(node[0])
+        m = node[0]
+    if 0 <= node[1] < len(grid[0]) and 0 <= node[2] < len(grid) and (node[3] < 4 and mmin(seen[node[1], node[2], node[4]]) > node[3] or node[3] >= 4 and mmax(seen[node[1], node[2], node[4]]) < 4):
+        if node[1] == len(grid[0]) - 1 and node[2] == len(grid) - 1 and node[3] >= 4:
+            print(node[0])
+            break
+        cnt = 0
+        if node[3] == 0:
+            for nsteps in range(1, 10 + 1):
+                nrow = node[1] + nsteps * node[4][0]
+                ncol = node[2] + nsteps * node[4][1]
+                if 0 <= nrow < len(grid[0]) and 0 <= ncol < len(grid):
+                    cnt += grid[nrow][ncol]
+                    if nsteps >= 4:
+                        q.append((node[0] + cnt, nrow, ncol, nsteps, node[4]))
+                else:
+                    break
+        if node[3] >= 4 and (node[4] == (1, 0) or node[4] == (-1, 0)):
+            for ndir in [(0, 1), (0, -1)]:
+                q.append((node[0], node[1], node[2], 0, ndir))
+        elif node[3] >= 4 and (node[4] == (0, 1) or node[4] == (0, -1)):
+            for ndir in [(1, 0), (-1, 0)]:
+                q.append((node[0], node[1], node[2], 0, ndir))
+        seen[(node[1], node[2], node[4])].add(node[3])
+        for x in q:
+            if not (x[3] < 4 and mmin(seen[x[1], x[2], x[4]]) > x[3] or x[3] >= 4 and mmax(seen[x[1], x[2], x[4]]) < 4):
+                q.remove(x)
+    node = min(q)
+
